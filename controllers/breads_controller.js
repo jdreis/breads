3f127c18ea6605149afc926f046d42cec4f49c48
Jena Reis <jenadreis@gmail.com>
2022-02-12 13:32:33 -0500

    sending index and showing data

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,6 +2,17 @@ const express = require('express');
 const router = express.Router()
 const Bread = require('../models/bread')
 
+// INDEX
+router.get('/', (req, res) => {
+    res.render('Index', 
+        {
+            breads: Bread
+        }
+    )
+    // res.send(Bread)
+})
+
+// SHOW
 router.get('/:id', (req, res) => {
     res.send(Bread[req.params.id])
 })
--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express()
 require('dotenv').config()
 const PORT = process.env.PORT || 3000
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 // ROUTES
 app.get('/', (req, res) => {
     res.send('welcome')
